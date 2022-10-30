f369e82a9e9922ed6c3bfd2d1c1011b8d035c651
CintrianWarrior <velegoj2@yandex.ru>
2022-10-30 17:06:23 +0300

    Task2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void defineTheLeapYear(int year) {
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
@@ -6,9 +8,27 @@ public class Main {
             System.out.println(year + " год не является високосным");
         }
     }
+
+    public static void defineOSApp(int clientOS, int clientDeviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        if (clientOS == 0 && clientDeviceYear<currentYear) {
+            System.out.println("Установите lite-версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear == currentYear) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1&& clientDeviceYear==currentYear) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        if (clientOS==1&&clientDeviceYear < currentYear){
+            System.out.println("Установите lite-версию приложения для Android по ссылке");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Задача 1");
         int year = 2020;
         defineTheLeapYear(year);
+        System.out.println("Задача 2");
+        int clientOS = 1;
+        int clientDeviceYear = 2022;
+        defineOSApp(clientOS,clientDeviceYear);
     }
 }
